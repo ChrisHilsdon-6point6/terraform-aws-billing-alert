c208ace3a3870126ab930537baf1f10c8450f2dd
Chris Hilsdon <chris.hilsdon@6point6.co.uk>
2023-10-31 15:51:41 +0000

    Added descriptions to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "aws_region" {
   type = string
+  description = "Specify the AWS region this alarm is deployed in"
 }
 
 variable "aws_access_key" {
   type = string
+  description = "AWS access key"
 }
 
 variable "aws_secret_key" {
   type = string
+  description = "AWS secret key"
 }
 
 variable "sns_alert_email" {
   type = string
+  description = "Valid email address alarm notifications get sent"
 }
