33204aec8868e7dbf94be515b469e253b3effb9c
Chris Hilsdon <chris.hilsdon@6point6.co.uk>
2023-11-06 15:02:50 +0000

    Update alarm description to force update

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ provider "aws" {
 
 resource "aws_cloudwatch_metric_alarm" "zero_spend_budget_alarm" {
   alarm_name = "zero_spend_budget"
-  alarm_description = "A CloudWatch Alarm to keep within AWS free tier limits."
+  alarm_description = "This alarm helps keep within AWS free tier limits."
   metric_name = "EstimatedCharges"
   namespace = "AWS/Billing"
   statistic = "Maximum"
   period = "${var.cw_alarm_period * 60 * 60}"
-  threshold = "0"
+  threshold = "0.01"
   evaluation_periods = "1"
   comparison_operator = "GreaterThanThreshold"
   alarm_actions = [aws_sns_topic.zero_spend_budget_alert.arn]
