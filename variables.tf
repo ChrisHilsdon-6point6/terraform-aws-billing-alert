8166d25431a9c1353e759310fc1ad75c26ebb385
Chris Hilsdon <chris.hilsdon@6point6.co.uk>
2023-11-06 14:55:12 +0000

    Mark AWS creds as sensitve to prevet exposure in Github PR comment

--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "aws_region" {
   type = string
+  sensitive = true
   description = "Specify the AWS region this alarm is deployed in"
 }
 
 variable "aws_access_key" {
   type = string
+  sensitive = true
   description = "AWS access key"
 }
 
 variable "aws_secret_key" {
   type = string
+  sensitive = true
   description = "AWS secret key"
 }
 
 variable "sns_alert_email" {
   type = string
+  sensitive = true
   description = "Valid email address alarm notifications get sent"
 }
 
